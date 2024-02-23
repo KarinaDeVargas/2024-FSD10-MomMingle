e5699fb9aefea155bc01d5dcdce57bf40ff9a488
KarinaDeVargas <134572496+KarinaDeVargas@users.noreply.github.com>
2024-02-23 15:07:58 -0500

    Inicial creation

--- mommingle/src/index.js
+++ mommingle/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './styles.css'; // Import the CSS file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
