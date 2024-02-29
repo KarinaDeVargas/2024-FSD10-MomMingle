5ce51c0f36fc9e9ba174eb0d8495f5642b19a268
Bo Kyung Kim <you@example.com>
2024-02-29 12:49:43 -0500

    server setting in progress

---
+++ mommingle/server/models/Users.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define("Users", {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Users;
+};
