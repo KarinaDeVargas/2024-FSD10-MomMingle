1a8fbfb28d69bf6ce344e0f3fca76b1df9f817cb
Bo Kyung Kim <you@example.com>
2024-02-29 13:46:44 -0500

    updated sever side code

--- mommingle/client/src/App.js
+++ mommingle/client/src/App.js
@@ -1,8 +1,4 @@
-import {
-  Outlet,
-  RouterProvider,
-  createBrowserRouter
-} from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -12,7 +8,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SearchEvent from "./pages/SearchEvent";
 import SingleEvent from "./pages/SingleEvent";
-import "./style.scss";
+import "./styles.css";
 
 const Layout = () => {
   return (
@@ -40,7 +36,7 @@ const router = createBrowserRouter([
       {
         path: "/searchevent",
         element: <SearchEvent />,
-      },      
+      },
       {
         path: "/createevent",
         element: <CreateEvent />,
@@ -69,14 +65,9 @@ function App() {
 
 export default App;
 
-
-
-
-
-
 // import React, { useState } from 'react';
 // import './styles.css'; // Import the CSS file
-// import 
+// import
 
 // // Header component
 // const Header = () => (
--- mommingle/server/index.js
+++ mommingle/server/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const db = require("./models");
 
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
     console.log("Server running on port 3001");
