bee4cb83024830871f4a0cca5f017c5d1d23e442
Bo Kyung Kim <you@example.com>
2024-03-03 22:29:42 -0500

    revised login page

--- mommingle/client/src/pages/Login.jsx
+++ mommingle/client/src/pages/Login.jsx
@@ -1,30 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  // const [inputs, setInputs] = useState({
-  //   username: "",
-  //   password: "",
-  // });
-  // const [err, setError] = useState(null);
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const { login } = useContext(AuthContext);
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-
-  // const handleChange = (e) => {
-  //   setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  // };
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     await login(inputs)
-  //     navigate("/");
-  //   } catch (err) {
-  //     setError(err.response.data);
-  //   }
-  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/login", inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
   return (
     <div className="auth">
       <h1>Login</h1>
@@ -33,21 +32,21 @@ const Login = () => {
           required
           type="text"
           placeholder="username"
-          // name="username"
-          // onChange={handleChange}
+          name="username"
+          onChange={handleChange}
         />
         <input
           required
           type="password"
           placeholder="password"
-          // name="password"
-          // onChange={handleChange}
+          name="password"
+          onChange={handleChange}
         />
-        {/* <button onClick={handleSubmit}>Login</button>
+        <button onClick={handleSubmit}>Login</button>
         {err && <p>{err}</p>}
         <span>
           Don't you have an account? <Link to="/register">Register</Link>
-        </span> */}
+        </span>
       </form>
     </div>
   );
