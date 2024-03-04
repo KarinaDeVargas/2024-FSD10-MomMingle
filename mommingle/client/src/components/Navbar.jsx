f00320072a715b783673614f21c0eb6acb265415
Bo Kyung Kim <you@example.com>
2024-03-04 00:05:22 -0500

    auth updated

--- mommingle/client/src/components/Navbar.jsx
+++ mommingle/client/src/components/Navbar.jsx
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-// import { AuthContext } from "../context/authContext";
+import { AuthContext } from "../context/authContext";
 import Logo from "../images/logo-no-background.png";
 import "../style/style_css.css";
 
-
 const Navbar = () => {
-  // const { currentUser, logout } = useContext(AuthContext);
+  const { currentUser, logout } = useContext(AuthContext);
 
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
           <Link to="/">
-          <img src={Logo} alt="" />
+            <img src={Logo} alt="" />
           </Link>
         </div>
         <div className="links">
           <Link className="link" to="/?cat=art">
-            <h6>ART</h6> 
+            <h6>ART</h6>
           </Link>
           {/* from "SCIENCE" to category "zumba" */}
           <Link className="link" to="/?cat=zumba">
@@ -33,22 +32,22 @@ const Navbar = () => {
           </Link>
           {/* from "design" to category "Playdate" */}
           <Link className="link" to="/?cat=playdate">
-            <h6>PLAYDATE</h6> 
+            <h6>PLAYDATE</h6>
           </Link>
           <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          {/* <span>{currentUser?.username}</span>
+          <span>{currentUser?.username}</span>
           {currentUser ? (
             <span onClick={logout}>Logout</span>
-          ) : ( */}
+          ) : (
             <Link className="link" to="/login">
               Login
             </Link>
-          {/* )} */}
+          )}
           <span className="write">
             <Link className="link" to="/createevent">
-            Create Event
+              Create Event
             </Link>
           </span>
         </div>
--- mommingle/client/src/pages/Login.jsx
+++ mommingle/client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
   const [inputs, setInputs] = useState({
@@ -11,6 +12,8 @@ const Login = () => {
 
   const navigate = useNavigate();
 
+  const { login } = useContext(AuthContext);
+
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -18,7 +21,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/auth/login", inputs);
+      await login(inputs);
       navigate("/");
     } catch (err) {
       setError(err.response.data);
