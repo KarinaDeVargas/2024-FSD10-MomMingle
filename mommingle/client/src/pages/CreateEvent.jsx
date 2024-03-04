6dcb6b0c857804b0aef51b48b9bc502340d410ef
Bo Kyung Kim <you@example.com>
2024-03-03 21:05:24 -0500

    Resolve merge conflicts

--- mommingle/client/src/pages/CreateEvent.jsx
+++ mommingle/client/src/pages/CreateEvent.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const CreateEvent = () => {
-  return (
-    <div>
-      CreateEvent
-    </div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default CreateEvent
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" id="file" name="" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Publish</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value="art" id="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="zumba" id="zumba" />
+            <label htmlFor="zumba">Zumba</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="meditation" id="meditation" />
+            <label htmlFor="meditation">Meditation</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="cinema" id="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="playdate" id="playdate" />
+            <label htmlFor="playdate">Playdate</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="food" id="food" />
+            <label htmlFor="food">Food</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateEvent;
--- mommingle/client/src/pages/SingleEvent.jsx
+++ mommingle/client/src/pages/SingleEvent.jsx
@@ -30,29 +30,26 @@ const Single = () => {
     fetchData();
   }, [postId]);
 
-  const handleDelete = async ()=>{
+  const handleDelete = async () => {
     try {
       await axios.delete(`/posts/${postId}`);
-      navigate("/")
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  const getText = (html) =>{
-    const doc = new DOMParser().parseFromString(html, "text/html")
-    return doc.body.textContent
-  }
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
 
   return (
     <div className="single">
       <div className="content">
         <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
-          {post.userImg && <img
-            src={post.userImg}
-            alt=""
-          />}
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
             <span>{post.username}</span>
             <p>Posted {moment(post.date).fromNow()}</p>
@@ -71,8 +68,9 @@ const Single = () => {
           dangerouslySetInnerHTML={{
             __html: DOMPurify.sanitize(post.desc),
           }}
-        ></p>      </div>
-      <Menu cat={post.cat}/>
+        ></p>
+      </div>
+      <Menu cat={post.cat} />
     </div>
   );
 };
